7ba7b5dd57233e6598f48a73b14676315c54da2e
贺佳男 <323240144@qq.com>
2022-03-29 18:15:34 -0400

    update the database configuration

--- backend/main/dao/user_dao.go
+++ backend/main/dao/user_dao.go
@@ -23,7 +23,7 @@ func (db *Database) FindUserByEmail(email string) pojo.User {
 }
 
 func InitUserDao() UserDao {
-	dsn := "root:softwareengineering@tcp(34.73.22.78:3306)/amazon?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := "root:123456@tcp(35.226.149.129:3306)/amazon?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic("Failed to connect database")

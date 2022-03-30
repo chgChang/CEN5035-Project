f3283c836309ea216f6eb194956ce7d2f86e978a
bakerston <czhang6@g.clemson.edu>
2022-03-29 23:41:17 -0400

    crud item

--- backend/main/service/item_service.go
+++ backend/main/service/item_service.go
@@ -43,6 +43,32 @@ func (service *itemService) SearchItemById(id int) (pojo.Item, error) {
 	return item, nil
 }
 
+//TODO
+func (service *itemService) AddItem(item pojo.Item) {
+	service.itemDao.CreateItem(item)
+}
+
+func (service *itemService) UpdateItem(item pojo.Item) error {
+	itemId := item.Id
+	originItem := service.itemDao.FindItemById(itemId)
+	if originItem == (pojo.Item{}) {
+		err := errors.New("id doesn't exist")
+		return err
+	}
+	service.itemDao.UpdateItem(item)
+	return nil
+}
+
+func (service *itemService) DeleteItem(itemId int) error {
+	originItem := service.itemDao.FindItemById(itemId)
+	if originItem == (pojo.Item{}) {
+		err := errors.New("item doesn't exist")
+		return err
+	}
+	service.itemDao.DeleteItem(itemId)
+	return nil
+}
+
 func NewItemService(itemDao dao.ItemDao) ItemService {
 	return &itemService{
 		itemDao: itemDao,

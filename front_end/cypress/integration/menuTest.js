aa23dc862c30b84f74d60a92fbcda1f5afa24805
chgChang <changz@mail.ustc.edu.cn>
2022-04-20 22:47:31 -0400

    fix bug

--- front_end/cypress/integration/menuTest.js
+++ front_end/cypress/integration/menuTest.js
@@ -1,24 +1,9 @@
-describe('register', function(){
+describe('menuTest', function(){
     this.beforeEach(()=>{
         cy.visit('http://localhost:8000/user/login')
-        cy.get('.ant-tabs-tab:contains("Register")').click()
-    })
-    it('newTest', function(){
-        cy.get('.ant-pro-form-login-main').get('#email').type('123@111.com')
-        cy.get('.ant-pro-form-login-main').get('#username').type('cc')
-        cy.get('.ant-pro-form-login-main').get('#registerPassword').type('zc1234')
-        cy.get('.ant-pro-form-login-main').get('#confirmPassword').type('zc1234')
+        cy.get('.ant-pro-form-login-main').get('#email').type('cz@cz.com')
+        cy.get('.ant-pro-form-login-main').get('#password').type('123456')
         cy.get('button:contains("Login")').click()
-        cy.url().should('eq', 'http://localhost:8000/')
-    })
-
-    it('existTest', function(){
-        cy.get('.ant-pro-form-login-main').get('#email').type('11@111.com')
-        cy.get('.ant-pro-form-login-main').get('#username').type('cc')
-        cy.get('.ant-pro-form-login-main').get('#registerPassword').type('zc1234')
-        cy.get('.ant-pro-form-login-main').get('#confirmPassword').type('zc1234')
-        cy.get('button:contains("Login")').click()
-        cy.url().should('include', '/user/login')
     })
 
     it('button', function(){
